999cafe205cca6941164b196290cdc4c832618e0
Kaoscillator <bader468@gmail.com>
2016-01-21 16:04:43 +0100

    Profil

--- Assignement_1/src/Main.java
+++ Assignement_1/src/Main.java
@@ -22,10 +22,22 @@ import javax.swing.JPopupMenu;
 import java.awt.Component;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import javax.swing.JList;
+import javax.swing.border.EtchedBorder;
+import javax.swing.AbstractListModel;
+import javax.swing.ListSelectionModel;
+import javax.swing.JMenu;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JComboBox;
 
 public class Main extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -49,19 +61,78 @@ public class Main extends JFrame {
 	public Main() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 572, 498);
-		
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(102, 153, 204));
+		contentPane.setBackground(new Color(255, 255, 204));
 		contentPane.setForeground(Color.WHITE);
 		contentPane.setToolTipText("");
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setLayout(null);
+		
+		table = new JTable();
+		table.setForeground(Color.GRAY);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"Andersson", "Jan"},
+				{"Bengtsson", "Sanne"},
+				{"Danske", "Liridon"},
+				{"Hansson", "Bruno"},
+				{"Herald", "Poppy"},
+				{"Isaksson", "Lars"},
+				{"Jazz", "Toni"},
+				{"Kittel", "Tom"},
+				{"Mauritzon", "Karin"},
+				{"M\u00E5rtensson", null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+			},
+			new String[] {
+				"New column", "New column"
+			}
+		));
+		table.setBounds(15, 43, 100, 205);
+		contentPane.add(table);
 		
 		ScrollPane scrollPane = new ScrollPane();
-		contentPane.add(scrollPane, BorderLayout.CENTER);
+		scrollPane.setBounds(15, 43, 100, 205);
+		contentPane.add(scrollPane);
+		
+		JMenu mnArkiv = new JMenu("ARKIV");
+		mnArkiv.setBounds(15, 13, 331, 24);
+		contentPane.add(mnArkiv);
+		
+		JLabel lblJanAndersson = new JLabel("Jan Andersson");
+		lblJanAndersson.setFont(new Font("Source Sans Pro Light", Font.PLAIN, 13));
+		lblJanAndersson.setBounds(407, 106, 106, 16);
+		contentPane.add(lblJanAndersson);
+		
+		JLabel lblNewLabel = new JLabel("620809-4098");
+		lblNewLabel.setFont(new Font("Source Sans Pro Light", Font.PLAIN, 13));
+		lblNewLabel.setBounds(407, 135, 106, 16);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblSrmlandsgatan = new JLabel("S\u00F6rmlandsgatan 12");
+		lblSrmlandsgatan.setFont(new Font("Source Sans Pro Light", Font.PLAIN, 13));
+		lblSrmlandsgatan.setBounds(407, 164, 135, 16);
+		contentPane.add(lblSrmlandsgatan);
+		
+		JLabel lblMalm = new JLabel("214 33 Malm\u00F6");
+		lblMalm.setFont(new Font("Source Sans Pro Light", Font.PLAIN, 13));
+		lblMalm.setBounds(407, 193, 106, 16);
+		contentPane.add(lblMalm);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\KGB\\Documents\\eclipse\\profil.jpg"));
+		lblNewLabel_1.setBounds(197, 43, 184, 205);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblJananderssongmailcom = new JLabel("jan.andersson@gmail.com");
+		lblJananderssongmailcom.setFont(new Font("Source Sans Pro Light", Font.PLAIN, 13));
+		lblJananderssongmailcom.setBounds(407, 222, 147, 16);
+		contentPane.add(lblJananderssongmailcom);
 	}
 
 	private static void addPopup(Component component, final JPopupMenu popup) {
