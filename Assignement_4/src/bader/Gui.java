9f1f014b96afc5c6fa007869798b20c115ce5dcf
Kaoscillator <bader468@gmail.com>
2016-02-10 14:12:01 +0100

    GUI DOG

--- Assignement_4/src/bader/Gui.java
+++ Assignement_4/src/bader/Gui.java
@@ -6,10 +6,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
 
 public class Gui extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -32,11 +37,20 @@ public class Gui extends JFrame {
 	 */
 	public Gui() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 745, 689);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(298, 26, 116, 22);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon(Gui.class.getResource("/bader/KennelGUI.png")));
+		lblNewLabel.setBounds(-153, 0, 972, 642);
+		contentPane.add(lblNewLabel);
 	}
-
 }
--- Assignement_4/src/bader/Human.java
+++ Assignement_4/src/bader/Human.java
@@ -42,4 +42,6 @@ public class Human {
 		}
      }
 
+return
+
 
