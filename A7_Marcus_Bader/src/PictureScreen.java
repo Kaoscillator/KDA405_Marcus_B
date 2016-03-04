5bae82e362ad761dbfcc8d2cd6eed54655a26dd5
Kaoscillator <bader468@gmail.com>
2016-03-04 11:58:27 +0100

    Ny Assignment 7
    Min final assignement 7 som jag lämnade in på Itslearning går inte att
    importera så jag gjorde en ny av en äldre version.  De är ungefär samma
    ändå. Ett svart streck med en liten böj.

--- A7_Marcus_Bader/src/PictureScreen.java
+++ A7_Marcus_Bader/src/PictureScreen.java
@@ -29,16 +29,45 @@ public class PictureScreen {
 			
 			System.out.println("size on screen" + px.getNumberPixelsWidestSide() + " : " + px.getNumberPixelsShortestSide());
 			
-			px.setPixel(10, 10, Color.BLUE);// int x int y, Color
-			px.setPixel(10, 11, Color.WHITE);
+			px.setPixel(10, 10, Color.BLACK);// int x int y, Color
+			px.setPixel(10, 11, Color.BLACK);
 			px.setPixel(10, 12, Color.BLACK);
-			px.setPixel(10, 13, Color.WHITE);
+			px.setPixel(10, 13, Color.BLACK);
 			px.setPixel(10, 14, Color.BLACK);
-			px.setPixel(10, 15, Color.WHITE);
+			px.setPixel(10, 15, Color.BLACK);
 			px.setPixel(10, 16, Color.BLACK);
-			px.setPixel(10, 17, Color.WHITE);
+			px.setPixel(10, 17, Color.BLACK);
 			px.setPixel(10, 18, Color.BLACK);
-			px.setPixel(10, 17, Color.WHITE);
+			px.setPixel(10, 17, Color.BLACK);
+			px.setPixel(10, 11, Color.BLACK);
+			px.setPixel(10, 12, Color.BLACK);
+			px.setPixel(10, 13, Color.BLACK);
+			px.setPixel(10, 14, Color.BLACK);
+			px.setPixel(10, 15, Color.BLACK);
+			px.setPixel(10, 16, Color.BLACK);
+			px.setPixel(10, 17, Color.BLACK);
+			px.setPixel(11, 18, Color.BLACK);
+			px.setPixel(12, 17, Color.BLACK);
+			px.setPixel(13, 18, Color.BLACK);
+			px.setPixel(14, 17, Color.BLACK);
+			px.setPixel(15, 11, Color.BLACK);
+			px.setPixel(16, 12, Color.BLACK);
+			px.setPixel(17, 13, Color.BLACK);
+			px.setPixel(18, 14, Color.BLACK);
+			px.setPixel(19, 15, Color.BLACK);
+			px.setPixel(20, 10, Color.BLACK);
+			px.setPixel(21, 11, Color.BLACK);
+			px.setPixel(22, 12, Color.BLACK);
+			px.setPixel(23, 13, Color.BLACK);
+			px.setPixel(15, 14, Color.BLACK);
+			px.setPixel(16, 13, Color.BLACK);
+			px.setPixel(17, 15, Color.BLACK);
+			px.setPixel(18, 16, Color.BLACK);
+			px.setPixel(19, 17, Color.BLACK);
+			px.setPixel(20, 18, Color.BLACK);
+			px.setPixel(21, 19, Color.BLACK);
+			px.setPixel(22, 20, Color.BLACK);
+			px.setPixel(23, 21, Color.BLACK);
 			
 		}
 	}
